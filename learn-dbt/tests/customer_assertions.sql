98b5466ccfc8c4ad1cd14072f238724d97ccc547
cbymar <chrisemsbu@gmail.com>
2021-04-05 15:12:56 -0700

    remove intentional test failure

--- learn-dbt/tests/customer_assertions.sql
+++ learn-dbt/tests/customer_assertions.sql
@@ -1,7 +1,7 @@
 
 WITH sum_c_acctbal AS 
 (SELECT 
-	{{ 0.999*var('max_acctbal_sum')}} + SUM(c_acctbal) AS acctbal_sum
+	SUM(c_acctbal) AS acctbal_sum
 FROM {{ ref('playing_with_tests') }}) 
 
 SELECT * FROM sum_c_acctbal WHERE acctbal_sum > {{ var('max_acctbal_sum')}}
