313629caaeeb9b3c5e2e0c4d270c26889d86126d
cbymar <chrisemsbu@gmail.com>
2021-04-05 15:31:14 -0700

    adding post hook just to first model

--- learn-dbt/models/example/my_first_dbt_model.sql
+++ learn-dbt/models/example/my_first_dbt_model.sql
@@ -7,7 +7,7 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table', alias='first_model') }}
+{{ config(materialized='table', alias='first_model', post_hook = 'grant select on {{this}} to role analyst') }}
 
 with source_data as (
 
