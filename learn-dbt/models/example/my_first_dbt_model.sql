745e50a1bd7c084d19dd3ac193479bf884c0d953
cbymar <chrisemsbu@gmail.com>
2021-04-05 20:02:55 -0700

    adding snapshot

--- learn-dbt/models/example/my_first_dbt_model.sql
+++ learn-dbt/models/example/my_first_dbt_model.sql
@@ -11,14 +11,14 @@
 
 with source_data as (
 
-    select 1 as id
-    -- union all
-    -- select null as id
-    -- union all
-    -- select 3 as id
+    select 1 as id, 'NY' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
+    union all
+    select null as id, 'CT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
+    union all
+    select 3 as id, 'VT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
 
 )
 
 select *
 from source_data
-where id is not null
+
